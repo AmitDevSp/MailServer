7e9dc30662874cb0d0531a7faa9c79d22d210112
AmitDevSp <amit.dev.sp@gmail.com>
2024-05-28 09:46:51 +0300

    Update app.ts - server ip get a variable from config file

--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
-import express from 'express'
 import dotenv from 'dotenv'
 import {MailRoutes} from  './routes/mailRoute'
+import express from 'express';
 const log = require('./helpers/loggerHelper')
 
 
 dotenv.config();
+const ip = process.env.SERVER_IP;
 const port = process.env.SERVER_PORT;
 const app = express();
 
@@ -14,6 +15,6 @@ app.use(MailRoutes);
 
 
 app.listen(port, () => {
-    log.info(`Server is running on http://localhost:${port}`)
-    console.log(`Server is running on http://localhost:${port}`)
+    log.info(`Server is running on http://${ip}:${port}`)
+    console.log(`Server is running on http://${ip}:${port}`)
 });
